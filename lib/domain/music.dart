4ca0cc15689c0de4450b6a8d0b31d24cad65f1f1
Eberson Oliveira <eberson.oliveira@gmail.com>
2023-08-09 20:39:05 -0300

    creating music app

---
+++ lib/domain/music.dart
@@ -0,0 +1,12 @@
+class Music{
+  final String title;
+  final int number;
+  final String duration;
+
+  Music({
+    required this.title,
+    required this.number,
+    required this. duration
+  });
+}
+
---
+++ lib/presentation/page/track_list_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TrackListPage extends StatelessWidget {
+  const TrackListPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Track List"),
+      ),
+    );
+  }
+}
