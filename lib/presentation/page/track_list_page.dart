2641c9ff94f551364a05c139348aa72546b10ded
Eberson Oliveira <eberson.oliveira@gmail.com>
2023-08-16 20:50:53 -0300

    finished music display layout

--- lib/presentation/page/track_list_page.dart
+++ lib/presentation/page/track_list_page.dart
@@ -11,44 +11,39 @@ class TrackListPage extends StatelessWidget {
     final album = ModalRoute.of(context)!.settings.arguments as Album;
 
     return Scaffold(
-      body: Container(
-        color: Colors.grey,
-        child: CustomScrollView(
-          slivers: [
-            SliverAppBar(
-              backgroundColor: Colors.black,
-              expandedHeight: 300,
-              flexibleSpace: FlexibleSpaceBar(
-                title: Text(
-                  album.title,
-                  style: theme.textTheme.headlineSmall?.copyWith(
-                    color: Colors.white,
-                    shadows: [
-                      const Shadow(
-                        color: Colors.grey,
-                        blurRadius: 4,
-                      )
-                    ],
-                  ),
-                ),
-                centerTitle: true,
-                collapseMode: CollapseMode.parallax,
-                background: Image.asset(
-                  album.cover,
-                  fit: BoxFit.cover,
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            backgroundColor: Colors.black,
+            expandedHeight: 300,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text(
+                album.title,
+                style: theme.textTheme.headlineSmall?.copyWith(
+                  color: Colors.white,
+                  shadows: [
+                    const Shadow(
+                      color: Colors.grey,
+                      blurRadius: 4,
+                    )
+                  ],
                 ),
               ),
-            ),
-            SliverList(            
-              delegate: SliverChildBuilderDelegate(
-                
-                (context, index) => album.musics.length > index ?
-                  DisplayMusic(music: album.musics[index]) : null
+              centerTitle: true,
+              collapseMode: CollapseMode.parallax,
+              background: Image.asset(
+                album.cover,
+                fit: BoxFit.cover,
               ),
             ),
-            
-          ],
-        ),
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate((context, index) =>
+                album.musics.length > index
+                    ? DisplayMusic(music: album.musics[index])
+                    : null),
+          ),
+        ],
       ),
     );
   }
